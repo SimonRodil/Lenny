b9d2632f193c94aa3545414aef3eae4085371d43
Simon Rodil <rodileduar.simon@gmail.com>
2026-03-29 11:08:26 +0200

    Update bot configuration with role and channel IDs
    Updated role and channel IDs for the bot configuration.

--- config.js
+++ config.js
@@ -29,36 +29,20 @@ module.exports = {
 
   // IDs de roles importantes (rellena con los de tu servidor)
   roles: {
-<<<<<<< HEAD
     admin:    '',
     modSenior:'',
     mod:      '',
     team:     '',
-=======
-    admin:    '793079297808138240',
-    modSenior:'793079297808138240',
-    mod:      '793079297808138240',
-    team:     '793079297808138240',
->>>>>>> 966b8a7 (Add initial configuration for the bot)
     verified: '',
   },
 
   // IDs de canales importantes
   channels: {
-<<<<<<< HEAD
     logs:         '',  // canal general (puedes dejarlo o quitarlo)
     welcome:      '',  // bienvenida (la desactivamos, pero dejamos la key)
     logsSuspect:  '',  // alertas de cuentas sospechosas
     logsSpam:     '',  // alertas de spam
     logsLinks:    '',  // alertas de links
     general:      '',  // alertas generales
-=======
-    logs:         '1487382936301863044',  // canal general (puedes dejarlo o quitarlo)
-    welcome:      '1487382936301863044',  // bienvenida (la desactivamos, pero dejamos la key)
-    logsSuspect:  '1487382936301863044',  // alertas de cuentas sospechosas
-    logsSpam:     '1487382936301863044',  // alertas de spam
-    logsLinks:    '1487382936301863044',  // alertas de links
-    general:      '1487382936301863044',  // alertas generales
->>>>>>> 966b8a7 (Add initial configuration for the bot)
   },
 };
